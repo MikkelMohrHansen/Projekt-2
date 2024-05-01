423c6ad12e41897adf1652912db0d015f4406e8d
minityhyi <andreashc@live.dk>
2024-05-01 08:59:20 +0200

    db virker

--- db/DBSimpel.sql
+++ db/DBSimpel.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE DBSimpel;
-use students;
+use DBSimpel;
 
 CREATE TABLE students(
     studentID int not null AUTO_INCREMENT,
-    firstName VARCHAR(50) NOT NULL;
-    surname VARCHAR(50) NOT NULL;
+    firstName VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
     PRIMARY KEY (studentID)
 );
 
 CREATE TABLE rooms(
     roomID int not NULL AUTO_INCREMENT,
-    roomName VARCHAR(50) NOT NULL;
+    roomName VARCHAR(50) NOT NULL,
     PRIMARY KEY (roomID)
 );
 
@@ -24,7 +24,7 @@ CREATE TABLE attendTable (
     FOREIGN KEY (studentID) REFERENCES students(studentID)
 );
 
-INSERT INTO students(firstName, surName)
+INSERT INTO students(firstName, surname)
 VALUES("Test", "Testson");
 
 INSERT INTO rooms(roomName)
