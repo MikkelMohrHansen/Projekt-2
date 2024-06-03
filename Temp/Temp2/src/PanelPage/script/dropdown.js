557a1e3e2b1936d2bf4fea2cff8356e7aac6a2d5
lucas fynbo <lucasfynbo@gmail.com>
2024-06-03 09:41:59 +0200

    Cleaned up temp folder

--- Temp/Temp2/src/PanelPage/script/dropdown.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var dropdownBtn = document.getElementById('dropdown_btn');
-    var dropdownOptions = document.getElementById('options');
-
-    dropdownBtn.addEventListener('click', function () {
-        dropdownOptions.classList.toggle('active');
-    });
-});
